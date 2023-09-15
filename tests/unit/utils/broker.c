e7113e7375fc7004dbd868e98a4472233be3ec5e
Karel Kočí <cynerd@email.cz>
2023-09-15 10:40:27 +0200

    tests/unit/broker: fix spawn of the broker
    For some reason the IPv6 is not bound automatically in the container.
    This changes both pyshvbroker to bound on generic localhost as well as
    the IPv6 only bound check to the more generic one.

--- tests/unit/utils/broker.c
+++ tests/unit/utils/broker.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <errno.h>
 #include <check.h>
 #include <spawn.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #include "tmpdir.h"
 
 int shvbroker_tcp_port;
@@ -16,33 +22,41 @@ static pid_t pid;
 
 
 static bool isbound(int port) {
-	bool res = false;
-	char *expect;
-	ck_assert_int_ne(
-		asprintf(&expect,
-			"00000000000000000000000000000000:%04X 00000000000000000000000000000000:0000",
-			shvbroker_tcp_port),
-		-1);
-	char *l = NULL;
-	size_t n;
-	FILE *f = fopen("/proc/net/tcp6", "r");
-	while (getline(&l, &n, f) != -1) {
-		if (strstr(l, expect) != NULL) {
-			res = true;
-			break;
-		}
+	struct addrinfo hints;
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;
+	struct addrinfo *addrs;
+	char *p;
+	ck_assert_int_ne(asprintf(&p, "%d", port), -1);
+	int res = getaddrinfo("localhost", p, &hints, &addrs);
+	free(p);
+	if (res != 0)
+		return false;
+
+	bool success = false;
+	struct addrinfo *addr;
+	int sock;
+	for (addr = addrs; !success && addr != NULL; addr = addr->ai_next) {
+		sock = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+		if (sock == -1)
+			continue;
+		if (connect(sock, addr->ai_addr, addr->ai_addrlen) != -1)
+			success = true;
+		close(sock);
 	}
-	fclose(f);
-	free(l);
-	free(expect);
-	return res;
+
+	freeaddrinfo(addrs);
+	return success;
 }
 
 static int empty_port(void) {
 	while (true) {
 		int res = 3600 + (rand() % 1000);
-		if (!isbound(res))
+		if (!isbound(res)) {
 			return res;
+		}
 	}
 }
 
@@ -54,7 +68,7 @@ void setup_shvbroker(void) {
 	ck_assert_ptr_nonnull(f);
 	fputs("[listen]\n", f);
 	shvbroker_tcp_port = empty_port();
-	fprintf(f, "internet = tcp://[::]:%d\n", shvbroker_tcp_port);
+	fprintf(f, "internet = tcp://localhost:%d\n", shvbroker_tcp_port);
 	shvbroker_unix_path = tmpdir_path("shvbroker.sock");
 	fprintf(f, "unix = localsocket:%s\n", shvbroker_unix_path);
 	fputs("[users.admin]\n", f);
@@ -73,6 +87,10 @@ void setup_shvbroker(void) {
 
 	ck_assert_int_eq(posix_spawn(&pid, PYSHVBROKER, NULL, NULL, args, NULL), 0);
 
+	/* Wait for unix socket to be ready */
+	struct stat stbuf;
+	while (stat(shvbroker_unix_path, &stbuf) == -1 && errno == ENOENT) {}
+	/* Wait for the TCP socket to be ready */
 	while (!isbound(shvbroker_tcp_port)) {}
 }
 
