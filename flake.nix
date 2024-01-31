9a20c62748c20a2e37371ff1241d769e37a66cf4
Karel Kočí <cynerd@email.cz>
2024-01-31 10:47:15 +0100

    flake.nix: rework
    This removes global with statements and adds app and other tweaks.

--- flake.nix
+++ flake.nix
@@ -12,117 +12,117 @@
     nixpkgs,
     semver,
     check-suite,
-  }:
-    with builtins;
-    with flake-utils.lib;
-    with nixpkgs.lib;
-    with semver.lib; let
-      version = changelog.currentRelease ./CHANGELOG.md self.sourceInfo;
-      src = builtins.path {
-        path = ./.;
-        filter = path: type: ! hasSuffix ".nix" path;
+  }: let
+    inherit (nixpkgs.lib) hasSuffix composeManyExtensions platforms;
+    inherit (flake-utils.lib) eachDefaultSystem filterPackages mkApp;
+    inherit (semver.lib) changelog;
+
+    version = changelog.currentRelease ./CHANGELOG.md self.sourceInfo;
+    src = builtins.path {
+      path = ./.;
+      filter = path: type: ! hasSuffix ".nix" path;
+    };
+
+    template-c = {
+      stdenv,
+      callPackage,
+      gperf,
+      meson,
+      ninja,
+      pkg-config,
+      doxygen,
+      sphinxHook,
+      python3Packages,
+      check,
+      check-suite,
+      bats,
+      bash,
+    }:
+      stdenv.mkDerivation {
+        pname = "template-c";
+        inherit version src;
+        GIT_REV = self.shortRev or self.dirtyShortRev;
+        outputs = ["out" "doc"];
+        buildInputs = [];
+        nativeBuildInputs = [
+          (callPackage ./subprojects/fetch.nix {
+            inherit src;
+            rev = self.rev or null;
+            hash = "sha256-R3YTQYHilh9R3eoyhhlyAuxU8BK9/c3DX1qTPKC+Y7E=";
+          })
+          gperf
+          meson
+          ninja
+          pkg-config
+          doxygen
+          (sphinxHook.overrideAttrs (oldAttrs: {
+            propagatedBuildInputs = with python3Packages; [
+              sphinx_rtd_theme
+              myst-parser
+              breathe
+            ];
+          }))
+        ];
+        checkInputs = [
+          check
+          check-suite
+        ];
+        nativeCheckInputs = [
+          bash
+          bats
+        ];
+        doCheck = true;
+        sphinxRoot = "../docs";
+      };
+  in
+    {
+      overlays = {
+        noInherit = final: prev: {
+          template-c = final.callPackage template-c {};
+        };
+        default = composeManyExtensions [
+          check-suite.overlays.default
+          self.overlays.noInherit
+        ];
+      };
+    }
+    // eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
+    in {
+      packages.default = pkgs.template-c;
+      legacyPackages = pkgs;
+
+      devShells = filterPackages system {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Linters and formaters
+            clang-tools_14
+            cppcheck
+            editorconfig-checker
+            flawfinder
+            muon
+            shellcheck
+            shfmt
+            gitlint
+            # Testing and code coverage
+            valgrind
+            gcovr
+            gcc11 # Hotfix for https://github.com/NixOS/nixpkgs/pull/279455
+            # Documentation
+            sphinx-autobuild
+          ];
+          inputsFrom = [self.packages.${system}.default];
+          meta.platforms = platforms.linux;
+        };
       };
 
-      template-c = {
-        stdenv,
-        callPackage,
-        gperf,
-        meson,
-        ninja,
-        pkg-config,
-        doxygen,
-        sphinxHook,
-        python3Packages,
-        check,
-        check-suite,
-        bats,
-        bash,
-      }:
-        stdenv.mkDerivation {
-          pname = "template-c";
-          inherit version src;
-          GIT_REV = self.shortRev or self.dirtyShortRev;
-          outputs = ["out" "doc"];
-          buildInputs = [];
-          nativeBuildInputs = [
-            (callPackage ./subprojects/fetch.nix {
-              inherit src;
-              rev = self.rev or null;
-              hash = "sha256-R3YTQYHilh9R3eoyhhlyAuxU8BK9/c3DX1qTPKC+Y7E=";
-            })
-            gperf
-            meson
-            ninja
-            pkg-config
-            doxygen
-            (sphinxHook.overrideAttrs (oldAttrs: {
-              propagatedBuildInputs = with python3Packages; [
-                sphinx_rtd_theme
-                myst-parser
-                breathe
-              ];
-            }))
-          ];
-          checkInputs = [
-            check
-            check-suite
-          ];
-          nativeCheckInputs = [
-            bash
-            bats
-          ];
-          doCheck = true;
-          sphinxRoot = "../docs";
-        };
-    in
-      {
-        foo = ./subprojects/.nix-setup-hook.sh;
-        overlays = {
-          noInherit = final: prev: {
-            template-c = final.callPackage template-c {};
-          };
-          default = composeManyExtensions [
-            check-suite.overlays.default
-            self.overlays.noInherit
-          ];
-        };
-      }
-      // eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackages.${system}.extend self.overlays.default;
-      in {
-        packages.default = pkgs.template-c;
-        legacyPackages = pkgs;
+      apps.default = mkApp {
+        drv = self.packages.${system}.default;
+        name = "foo";
+      };
 
-        devShells = filterPackages system {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              # Linters and formaters
-              clang-tools_14
-              cppcheck
-              editorconfig-checker
-              flawfinder
-              muon
-              shellcheck
-              shfmt
-              gitlint
-              # Testing and code coverage
-              valgrind
-              gcovr
-              # Documentation
-              sphinx-autobuild
-            ];
-            inputsFrom = [self.packages.${system}.default];
-            meta.platforms = platforms.linux;
-          };
-        };
+      checks.default = self.packages.${system}.default;
 
-        apps.default = {
-          type = "app";
-          program = "${self.packages.${system}.default}/bin/foo";
-        };
-
-        checks.default = self.packages.${system}.default;
-
-        formatter = pkgs.alejandra;
-      });
+      formatter = pkgs.alejandra;
+    });
 }
