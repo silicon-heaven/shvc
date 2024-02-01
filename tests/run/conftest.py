da9238682bb6f8018bd4c5e9682fc009b4b4db75
Karel Kočí <cynerd@email.cz>
2024-02-01 11:19:11 +0100

    cpconv: remove
    We have this tool in Python version. We gain nothing by implementing it
    in C as well. It is better to maintain only one version.

--- tests/run/conftest.py
+++ tests/run/conftest.py
@@ -1,24 +1,7 @@
 import os
 import shlex
-import subprocess
-import sys
-import time
 
 import pytest
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--cpconv",
-        help="Path to cpconv device binary",
-        metavar="PATH",
-        nargs=1,
-    )
-
-
-def pytest_configure(config):
-    cpconv = config.getoption("--cpconv")
-    setattr(config, "cpconv", cpconv[0] if cpconv else None)
 
 
 @pytest.fixture(name="valgrind_exec", scope="session")
