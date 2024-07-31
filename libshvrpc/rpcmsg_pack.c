66e52699fd86da78571f3b3895754f683f7f2a5b
Karel Kočí <kkoci@elektroline.cz>
2024-07-31 17:17:57 +0200

    libshvrpc/rpcmsg_pack: detect packing error only on last packer call
    This simplifies code on the bases of changes introduced in
    f362b3bf0bcc3ce571cbb33e2fa6ff81293d243f.

--- libshvrpc/rpcmsg_pack.c
+++ libshvrpc/rpcmsg_pack.c
@@ -2,132 +2,116 @@
 #include <string.h>
 #include <shv/rpcmsg.h>
 
-#define G(V) \
-	do { \
-		if (!(V)) \
-			return false; \
-	} while (false)
-
 
 static inline bool meta_begin(cp_pack_t pack) {
-	G(cp_pack_meta_begin(pack));
-	G(cp_pack_int(pack, RPCMSG_TAG_META_TYPE_ID));
-	G(cp_pack_int(pack, 1));
-	return true;
+	cp_pack_meta_begin(pack);
+	cp_pack_int(pack, RPCMSG_TAG_META_TYPE_ID);
+	return cp_pack_int(pack, 1);
 }
 
 __attribute__((nonnull(1, 2, 3))) bool _rpcmsg_pack_request(cp_pack_t pack,
 	const char *path, const char *method, const char *uid, int rid) {
-	G(meta_begin(pack));
-	G(cp_pack_int(pack, RPCMSG_TAG_REQUEST_ID));
-	G(cp_pack_int(pack, rid));
-	G(cp_pack_int(pack, RPCMSG_TAG_SHV_PATH));
-	G(cp_pack_str(pack, path ?: ""));
-	G(cp_pack_int(pack, RPCMSG_TAG_METHOD));
-	G(cp_pack_str(pack, method));
+	meta_begin(pack);
+	cp_pack_int(pack, RPCMSG_TAG_REQUEST_ID);
+	cp_pack_int(pack, rid);
+	cp_pack_int(pack, RPCMSG_TAG_SHV_PATH);
+	cp_pack_str(pack, path ?: "");
+	cp_pack_int(pack, RPCMSG_TAG_METHOD);
+	cp_pack_str(pack, method);
 	if (uid) {
-		G(cp_pack_int(pack, RPCMSG_TAG_USER_ID));
-		G(cp_pack_str(pack, uid));
+		cp_pack_int(pack, RPCMSG_TAG_USER_ID);
+		cp_pack_str(pack, uid);
 	}
-	G(cp_pack_container_end(pack));
+	cp_pack_container_end(pack);
 
-	G(cp_pack_imap_begin(pack));
-	return true;
+	return cp_pack_imap_begin(pack);
 }
 
 bool rpcmsg_pack_request(cp_pack_t pack, const char *path, const char *method,
 	const char *uid, int rid) {
-	G(_rpcmsg_pack_request(pack, path, method, uid, rid));
-	G(cp_pack_int(pack, RPCMSG_KEY_PARAM));
-	return true;
+	_rpcmsg_pack_request(pack, path, method, uid, rid);
+	return cp_pack_int(pack, RPCMSG_KEY_PARAM);
 }
 
 bool rpcmsg_pack_request_void(cp_pack_t pack, const char *path,
 	const char *method, const char *uid, int rid) {
-	G(_rpcmsg_pack_request(pack, path, method, uid, rid));
-	G(cp_pack_container_end(pack));
-	return true;
+	_rpcmsg_pack_request(pack, path, method, uid, rid);
+	return cp_pack_container_end(pack);
 }
 
 static bool _rpcmsg_pack_signal(cp_pack_t pack, const char *path,
 	const char *source, const char *signal, const char *uid, rpcaccess_t access) {
-	G(meta_begin(pack));
-	G(cp_pack_int(pack, RPCMSG_TAG_SHV_PATH));
-	G(cp_pack_str(pack, path ?: ""));
+	meta_begin(pack);
+	cp_pack_int(pack, RPCMSG_TAG_SHV_PATH);
+	cp_pack_str(pack, path ?: "");
 	/* Note: We always pack signal name for pre-SHV 3.0 compatibility */
-	G(cp_pack_int(pack, RPCMSG_TAG_SIGNAL));
-	G(cp_pack_str(pack, signal));
+	cp_pack_int(pack, RPCMSG_TAG_SIGNAL);
+	if (!cp_pack_str(pack, signal))
+		return false;
 	if (strcmp(source, "get")) {
-		G(cp_pack_int(pack, RPCMSG_TAG_SOURCE));
-		G(cp_pack_str(pack, source));
+		cp_pack_int(pack, RPCMSG_TAG_SOURCE);
+		cp_pack_str(pack, source);
 	}
 	if (uid) {
-		G(cp_pack_int(pack, RPCMSG_TAG_USER_ID));
-		G(cp_pack_str(pack, uid));
+		cp_pack_int(pack, RPCMSG_TAG_USER_ID);
+		cp_pack_str(pack, uid);
 	}
 	if (access != RPCACCESS_READ) {
-		G(cp_pack_int(pack, RPCMSG_TAG_ACCESS_LEVEL));
-		G(cp_pack_int(pack, access));
+		cp_pack_int(pack, RPCMSG_TAG_ACCESS_LEVEL);
+		cp_pack_int(pack, access);
 	}
-	G(cp_pack_container_end(pack));
+	cp_pack_container_end(pack);
 
-	G(cp_pack_imap_begin(pack));
-	return true;
+	return cp_pack_imap_begin(pack);
 }
 
 bool rpcmsg_pack_signal(cp_pack_t pack, const char *path, const char *source,
 	const char *signal, const char *uid, rpcaccess_t access) {
-	G(_rpcmsg_pack_signal(pack, path, source, signal, uid, access));
-	G(cp_pack_int(pack, RPCMSG_KEY_PARAM));
-	return true;
+	_rpcmsg_pack_signal(pack, path, source, signal, uid, access);
+	return cp_pack_int(pack, RPCMSG_KEY_PARAM);
 }
 
 bool rpcmsg_pack_signal_void(cp_pack_t pack, const char *path,
 	const char *source, const char *signal, const char *uid, rpcaccess_t access) {
-	G(_rpcmsg_pack_signal(pack, path, source, signal, uid, access));
-	G(cp_pack_container_end(pack));
-	return true;
+	_rpcmsg_pack_signal(pack, path, source, signal, uid, access);
+	return cp_pack_container_end(pack);
 }
 
 static bool _pack_response(cp_pack_t pack, const struct rpcmsg_meta *meta) {
-	G(meta_begin(pack));
-	G(cp_pack_int(pack, RPCMSG_TAG_REQUEST_ID));
-	G(cp_pack_int(pack, meta->request_id));
+	meta_begin(pack);
+	cp_pack_int(pack, RPCMSG_TAG_REQUEST_ID);
+	cp_pack_int(pack, meta->request_id);
 	if (meta->cids_cnt) {
-		G(cp_pack_int(pack, RPCMSG_TAG_CALLER_IDS));
+		cp_pack_int(pack, RPCMSG_TAG_CALLER_IDS);
 		if (meta->cids_cnt > 1) {
-			G(cp_pack_list_begin(pack));
+			cp_pack_list_begin(pack);
 			for (size_t i = 0; i < meta->cids_cnt; i++)
-				G(cp_pack_int(pack, meta->cids[i]));
-			G(cp_pack_container_end(pack));
+				cp_pack_int(pack, meta->cids[i]);
+			cp_pack_container_end(pack);
 		} else
-			G(cp_pack_int(pack, meta->cids[0]));
+			cp_pack_int(pack, meta->cids[0]);
 	}
-	G(cp_pack_container_end(pack));
+	cp_pack_container_end(pack);
 
-	G(cp_pack_imap_begin(pack));
-	return true;
+	return cp_pack_imap_begin(pack);
 }
 
 bool rpcmsg_pack_response(cp_pack_t pack, const struct rpcmsg_meta *meta) {
-	G(_pack_response(pack, meta));
-	G(cp_pack_int(pack, RPCMSG_KEY_RESULT));
-	return true;
+	_pack_response(pack, meta);
+	return cp_pack_int(pack, RPCMSG_KEY_RESULT);
 }
 
 bool rpcmsg_pack_response_void(cp_pack_t pack, const struct rpcmsg_meta *meta) {
-	G(_pack_response(pack, meta));
-	G(cp_pack_container_end(pack));
-	return true;
+	_pack_response(pack, meta);
+	return cp_pack_container_end(pack);
 }
 
 bool rpcmsg_pack_error(cp_pack_t pack, const struct rpcmsg_meta *meta,
 	rpcerrno_t errnum, const char *msg) {
-	G(_pack_response(pack, meta));
-	G(cp_pack_int(pack, RPCMSG_KEY_ERROR));
-	G(rpcerror_pack(pack, errnum, msg));
-	G(cp_pack_container_end(pack));
-	return true;
+	_pack_response(pack, meta);
+	cp_pack_int(pack, RPCMSG_KEY_ERROR);
+	rpcerror_pack(pack, errnum, msg);
+	return cp_pack_container_end(pack);
 }
 
 bool rpcmsg_pack_ferror(cp_pack_t pack, const struct rpcmsg_meta *meta,
@@ -141,8 +125,9 @@ bool rpcmsg_pack_ferror(cp_pack_t pack, const struct rpcmsg_meta *meta,
 
 bool rpcmsg_pack_vferror(cp_pack_t pack, const struct rpcmsg_meta *meta,
 	rpcerrno_t errnum, const char *fmt, va_list args) {
-	G(_pack_response(pack, meta));
-	G(cp_pack_int(pack, RPCMSG_KEY_ERROR));
+	_pack_response(pack, meta);
+	if (!cp_pack_int(pack, RPCMSG_KEY_ERROR))
+		return false;
 
 	va_list cargs;
 	va_copy(cargs, args);
@@ -152,8 +137,7 @@ bool rpcmsg_pack_vferror(cp_pack_t pack, const struct rpcmsg_meta *meta,
 	va_end(cargs);
 	char msg[siz + 1];
 	assert(vsnprintf(msg, siz + 1, fmt, args) == siz);
-	G(rpcerror_pack(pack, errnum, msg));
+	rpcerror_pack(pack, errnum, msg);
 
-	G(cp_pack_container_end(pack));
-	return true;
+	return cp_pack_container_end(pack);
 }
