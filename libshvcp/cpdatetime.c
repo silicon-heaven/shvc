6e86be47cd1456e429d711b47ad894729d077637
Michal Lenc <michallenc@seznam.cz>
2024-11-25 11:27:45 +0100

    cpdatetime: return time relative to the user's specified timezone
    The function is used to obtain time for .app:date method that should
    return the date and time the application is using. This includes
    the user's specified timezone. Therefore the function should use
    localtime_r instead of gmtime_r to obtain correct time from the system.
    
    Signed-off-by: Michal Lenc <michallenc@seznam.cz>

--- libshvcp/cpdatetime.c
+++ libshvcp/cpdatetime.c
@@ -20,7 +20,7 @@ struct cpdatetime clock_cpdatetime(void) {
 	struct timespec ts;
 	clock_gettime(CLOCK_REALTIME, &ts);
 	struct tm tm;
-	gmtime_r(&ts.tv_sec, &tm);
+	localtime_r(&ts.tv_sec, &tm);
 	return (struct cpdatetime){
 		.msecs = (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000),
 		.offutc = tm.tm_gmtoff / 60,
