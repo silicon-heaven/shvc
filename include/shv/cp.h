silicon-heaven/shvc

commit eae2285511d4d8b70e2343f3431410b403b1c387

libshvcp: add few more decimal utilities
This adds cpdecexp, cpdtoi, and cpitod.

The cpdeccmp and cpdtodec are removed. The cpdeccmp had invalid
implementation and cpdtodec was never actually provided.

The implementation of cpitem_extract_int and cpitem_unpack_uinit were
simplified as we can use native value cropping instead of smart
calculations.

include/shv/cp.h
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <sys/types.h>
+#include <assert.h>
 #include <time.h>
 
 
@@ -147,22 +148,20 @@ struct cpdecimal {
  */
 void cpdecnorm(struct cpdecimal *v) __attribute__((nonnull));
 
-/*! Compare two decimal numbers.
+/*! Modify decimal number to be at given exponent.
  *
- * The input must be normalized, otherwise assert is tripped.
+ * The lost precision is never reported and value is cropped instead of
+ * rounded!
  *
- * @returns a positive number if ``A`` is greater than ``B``, a negative number
- *   ``A`` is less than ``B`` and zero if ``A`` is equal to ``B``.
+ * The decimal is modified even if `false` is returned. It only stops at
+ * exponent that still not cause `intmax_t` overflow.
+ *
+ * @param v Decimal number to be modified.
+ * @param exponent The desired exponent number.
+ * @returns `true` if set correctly and `false` if exponent can't be reached due
+ * to mantisa overflow.
  */
-#define cpdeccmp(A, B) \
-	({ \
-		assert(A->mantisa % 10 || A->mantisa == 0); \
-		assert(B->mantisa % 10 || B->mantisa == 0); \
-		intmax_t res = A->exponent - B->exponment; \
-		if (res == 0) \
-			res = A->mantisa - B->mantisa; \
-		res; \
-	})
+bool cpdecexp(struct cpdecimal *v, int exponent) __attribute__((nonnull));
 
 /*! Convert decimal number to double precision floating point number.
  *
@@ -171,12 +170,41 @@ void cpdecnorm(struct cpdecimal *v) __attribute__((nonnull));
  */
 double cpdectod(const struct cpdecimal v);
 
-/*! Convert double precision floating point number to decimal number.
+/*! Convert decimal number to integer with given multiplication.
  *
- * @param v Floating point number to be converted.
- * @returns Approximate decimal representation.
+ * This is commonly used if your application works with integers but values sent
+ * in SHV are in fractional units (for example you have milliseconds while SHV
+ * sends decimal seconds).
+ *
+ * @param DEC The decimal to be converted (variable not pointer to it).
+ * @param EXP The exponent of the destination integer.
  */
-struct cpdecimal cpdtodec(double v);
+#define cpdtoi(DEC, EXP, DEST) \
+	({ \
+		bool __dec_valid = false; \
+		struct cpdecimal __dec = (DEC); \
+		if (cpdecexp(&__dec, (EXP))) { \
+			(DEST) = __dec.mantisa; \
+			__dec_valid = __dec.mantisa == (DEST); \
+		} \
+		__dec_valid; \
+	})
+
+/*! Convert given mantisa and exponent to decimal.
+ *
+ * This automatically normalizes the newly created decimal. Otherwise there is
+ * no advantage in using this over direct structure initialization.
+ *
+ * @param MANTISA The mantisa to be used.
+ * @param EXPONENT The exponent (current multiplication of mantisa).
+ * @returns The normalized decimal number.
+ */
+#define cpitod(MANTISA, EXPONENT) \
+	({ \
+		struct cpdecimal __dec_res = {.mantisa = MANTISA, .exponent = EXPONENT}; \
+		cpdecnorm(&__dec_res); \
+		__dec_res; \
+	})
 
 
 /*! Representation of date and time including the time zone offset. */
@@ -444,17 +472,7 @@ static inline void cpitem_unpack_init(struct cpitem *item) {
 		bool __valid = false; \
 		if (__item->type == CPITEM_INT) { \
 			(DEST) = __item->as.Int; \
-			if ((typeof(DEST))-1 < 0) { \
-				const intmax_t __lim = \
-					((((typeof(DEST))1 << (sizeof(DEST) * 8 - 2)) - 1) << 1) + 1; \
-				__valid = __item->as.Int <= __lim && \
-					__item->as.Int >= (-__lim - 1); \
-			} else { \
-				const intmax_t __lim = sizeof(DEST) < sizeof(intmax_t) \
-					? (typeof(DEST))~(typeof(DEST))0 \
-					: INTMAX_MAX; \
-				__valid = __item->as.Int <= __lim && __item->as.Int >= 0; \
-			} \
+			__valid = __item->as.Int == (DEST); \
 		} \
 		__valid; \
 	})
@@ -479,10 +497,7 @@ static inline void cpitem_unpack_init(struct cpitem *item) {
 		bool __valid = false; \
 		if (__item->type == CPITEM_UINT) { \
 			(DEST) = __item->as.UInt; \
-			const uintmax_t __lim = (typeof(DEST))-1 < 0 \
-				? ((((typeof(DEST))1 << (sizeof(DEST) * 8 - 2)) - 1) << 1) + 1 \
-				: (typeof(DEST))~(typeof(DEST))0; \
-			__valid = __item->as.UInt <= __lim; \
+			__valid = __item->as.UInt == (DEST); \
 		} \
 		__valid; \
 	})

libshvcp/cpdecimal.c
@@ -1,4 +1,3 @@
-#include <math.h>
 #include <shv/cp.h>
 
 void cpdecnorm(struct cpdecimal *v) {
@@ -12,6 +11,21 @@ void cpdecnorm(struct cpdecimal *v) {
 	}
 }
 
+bool cpdecexp(struct cpdecimal *v, int exponent) {
+	int neg = v->exponent < exponent ? 1 : -1;
+	while (v->exponent != exponent && v->mantisa) {
+		long long int mantisa;
+		if (neg < 0) {
+			if (__builtin_smulll_overflow(v->mantisa, 10, &mantisa))
+				return false;
+		} else
+			mantisa = v->mantisa / 10;
+		v->mantisa = mantisa;
+		v->exponent += neg;
+	}
+	return true;
+}
+
 double cpdectod(const struct cpdecimal v) {
 	double res = v.mantisa;
 	if (v.exponent >= 0) {
@@ -23,16 +37,3 @@ double cpdectod(const struct cpdecimal v) {
 	}
 	return res;
 }
-
-#if 0
-struct cpdecimal cpdtodec(double v) {
-	// TODO
-	double i;
-	double d = modf(v, &i);
-	printf("That would be %f and %f\n", i, d);
-	int exp;
-	double s = frexp(v, &exp);
-	printf("frexp %d for %lf\n", exp, s);
-	return (struct cpdecimal) {.mantisa = i, .exponent = d};
-}
-#endif

libshvcp/libshvcp.version
@@ -3,8 +3,8 @@ V0.0 {
 		# shv/cp.h
 		cpitem_type_str;
 		cpdecnorm;
+		cpdecexp;
 		cpdectod;
-		cpdtodec;
 		cpdttotm;
 		cptmtodt;
 		clock_cpdatetime;

tests/unit/libshvcp/cpdecimal.c
@@ -31,6 +31,7 @@ static const struct {
 	{1., (struct cpdecimal){.mantisa = 1}},
 	{-1., (struct cpdecimal){.mantisa = -1}},
 	{42.124, (struct cpdecimal){.mantisa = 42124, .exponent = -3}},
+	{400., (struct cpdecimal){.mantisa = 4, .exponent = 2}},
 };
 
 ARRAY_TEST(all, dectod, dec_d) {
@@ -38,11 +39,59 @@ ARRAY_TEST(all, dectod, dec_d) {
 }
 END_TEST
 
-#if 0
-ARRAY_TEST(all, dtodec, _d) {
-	struct cpdecimal d = cpdtodec(_d.f);
-	ck_assert_int_eq(d.mantisa, _d.d.mantisa);
-	ck_assert_int_eq(d.exponent, _d.d.exponent);
+static const struct {
+	struct cpdecimal in, out;
+	int exponent;
+} decexp_d[] = {
+	{{42, 3}, {42, 3}, 3},
+	{{42, 3}, {42000, 0}, 0},
+	{{42, 3}, {420, 2}, 2},
+	{{42, 3}, {4200000, -2}, -2},
+	{{42, 3}, {4, 4}, 4},
+	{{42, 3}, {0, 5}, 5},
+	{{42, 3}, {0, 5}, 6},
+};
+ARRAY_TEST(all, decexp) {
+	struct cpdecimal d = _d.in;
+	ck_assert(cpdecexp(&d, _d.exponent));
+	ck_assert_int_eq(d.mantisa, _d.out.mantisa);
+	ck_assert_int_eq(d.exponent, _d.out.exponent);
 }
-END_TEST
-#endif
+
+static const struct {
+	struct cpdecimal dec;
+	int exponent;
+} decexp_overflow_d[] = {
+	{{42, 3}, -18},
+};
+ARRAY_TEST(all, decexp_overflow) {
+	struct cpdecimal d = _d.dec;
+	ck_assert(!cpdecexp(&d, _d.exponent));
+}
+
+static const struct {
+	struct cpdecimal dec;
+	int exp, val;
+} cpdtoi_d[] = {
+	{{42, 0}, -3, 42000},
+	{{42, -2}, -3, 420},
+	{{42, -4}, -3, 4},
+};
+ARRAY_TEST(all, cpdtoi) {
+	int val;
+	ck_assert(cpdtoi(_d.dec, _d.exp, val));
+	ck_assert_int_eq(val, _d.val);
+}
+
+static const struct {
+	int mantisa, exponent;
+	struct cpdecimal dec;
+} cpitod_d[] = {
+	{42, -3, {42, -3}},
+	{4200, -3, {42, -1}},
+};
+ARRAY_TEST(all, cpitod) {
+	struct cpdecimal d = cpitod(_d.mantisa, _d.exponent);
+	ck_assert_int_eq(d.mantisa, _d.dec.mantisa);
+	ck_assert_int_eq(d.exponent, _d.dec.exponent);
+}

commit 0e099c16729182470b475cf14a1f5e0b964bc592

libshvcp: fix expansion side effect for cp.h macros
The commit eae2285511d4d8b70e2343f3431410b403b1c387 introduced issue
where side effect of DEST expansion is applied multiple times due to
multiple usage of DEST.

The usage of DEST in typeof doesn't have side effect thus the modified
code expands the DEST code effectively only once.

include/shv/cp.h
@@ -184,10 +184,11 @@ double cpdectod(const struct cpdecimal v);
 #define cpdtoi(DEC, EXP, DEST) \
 	({ \
 		bool __dec_valid = false; \
+		typeof(DEST) *__dest = &(DEST); \
 		struct cpdecimal __dec = (DEC); \
 		if (cpdecexp(&__dec, (EXP))) { \
-			(DEST) = __dec.mantisa; \
-			__dec_valid = __dec.mantisa == (DEST); \
+			*__dest = __dec.mantisa; \
+			__dec_valid = __dec.mantisa == *__dest; \
 		} \
 		__dec_valid; \
 	})
@@ -474,7 +475,7 @@ static inline void cpitem_unpack_init(struct cpitem *item) {
 		bool __valid = false; \
 		if (__item->type == CPITEM_INT) { \
 			(DEST) = __item->as.Int; \
-			__valid = __item->as.Int == (DEST); \
+			__valid = __item->as.Int == (typeof(DEST))__item->as.Int; \
 		} \
 		__valid; \
 	})
@@ -499,7 +500,7 @@ static inline void cpitem_unpack_init(struct cpitem *item) {
 		bool __valid = false; \
 		if (__item->type == CPITEM_UINT) { \
 			(DEST) = __item->as.UInt; \
-			__valid = __item->as.UInt == (DEST); \
+			__valid = __item->as.UInt == (typeof(DEST))__item->as.UInt; \
 		} \
 		__valid; \
 	})